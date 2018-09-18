89590c46a844467b02d6b46cacc426e12af80f24
Volodymyr <volodymyr.dubinskyi@gmail.com>
2018-09-18 10:19:41 +0300

    finish with tests

--- ui/src/components/__tests__/List/TodoItem.test.js
+++ ui/src/components/__tests__/List/TodoItem.test.js
@@ -11,8 +11,8 @@ beforeEach(() => {
   status = ''
   wrapper = enzyme.shallow(
     <TodoItem
-      todo={{ title: 'old title', id: 512952 }}
-      removeToDo={() => { }}
+      todo={{ title: 'old title', id: 512952, chosen: false }}
+      removeToDo={(listId, todoId) => { status = `ToDo ${todoId} in list ${listId} removed` }}
       listId={92573}
       editToDo={(listId, todoId, changes) => { status = { listId, todoId, changes } }}
     />,
@@ -21,6 +21,12 @@ beforeEach(() => {
 
 it('rendered', () => {
   expect(wrapper.exists()).toBe(true);
+})
+
+it('go to edit menu', () => {
+  wrapper.instance().goToEdit()
+
+  expect(wrapper.state('todoNowEditting')).toBe(true);
 })
 
 it('try to edit title', () => {
@@ -35,7 +41,63 @@ it('try to edit title', () => {
 })
 
 it('Save and stop edditing', () => {
+  const fakeEvent = {
+    target: {
+      value: 'Some new title',
+    },
+  }
+  wrapper.instance().updateEditValue(fakeEvent)
+
   wrapper.instance().saveAndStopEditing()
 
-  expect(wrapper.state('editValue')).toBe('Some new title');
+  expect(wrapper.state('todoNowEditting')).toBe(false);
+  expect(status.changes.title).toBe('Some new title');
+  expect(status.todoId).toBe(512952);
+  expect(status.listId).toBe(92573);
+})
+
+it('Save and stop edditing using enter', () => {
+  const fakeEvent = {
+    target: {
+      value: 'Some new title',
+    },
+    keyCode: 13,
+  }
+  wrapper.instance().updateEditValue(fakeEvent)
+
+  wrapper.instance().saveAndStopEdditingUsingEnter(fakeEvent)
+
+  expect(wrapper.state('todoNowEditting')).toBe(false);
+  expect(status.changes.title).toBe('Some new title');
+  expect(status.todoId).toBe(512952);
+  expect(status.listId).toBe(92573);
+})
+
+it('Stop edditing without save', () => {
+  const fakeEvent = {
+    target: {
+      value: 'Some new title',
+    },
+  }
+  wrapper.instance().updateEditValue(fakeEvent)
+
+  wrapper.instance().stopEditing()
+
+  expect(wrapper.state('todoNowEditting')).toBe(false);
+  expect(status).toBe('');
+})
+
+it('Remove todo', () => {
+  wrapper.instance().deleteTodo()
+
+  expect(status).toBe('ToDo 512952 in list 92573 removed');
+})
+
+it('Toggle todo', () => {
+  wrapper.instance().toggleChose()
+
+  expect(wrapper.state('todoNowEditting')).toBe(false);
+  expect(status.changes.chosen).toBe(true);
+  expect(status.todoId).toBe(512952);
+  expect(status.listId).toBe(92573);
 })
